7668136c882d66ac36284973c5afba8b5fd52812
MFranckFR <76939469+MFranckFR@users.noreply.github.com>
2021-02-05 18:54:15 +0100

    Intro: binding, boucles, conditions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Personne } from './classes/personne';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angNE1 Mon appli';
+  personne: Personne = new Personne(100, 'John', 'Doe');
+  personnes: Personne[] = [
+    new Personne(100, 'John', 'Doe'), 
+    new Personne(200, 'John', 'John'),
+    new Personne(300, 'John', 'Kennedy'),
+    new Personne(400, 'John', 'Wayne'),
+  ];
+  tab: number[] = Array.from([1,2,3,4,5,6,7,8], x=>Math.floor(Math.random()*21));
+  libelleFormation = "POEC Font-EndS";
+  couleur = "blue";
+  nom = this.getRandomPersonne().name;
+
+  getColor(){
+    //couleurs:string[] = Array('red', 'blue', 'yellow', 'green', 'orange', 'pink', 'darkblue');
+    //couleurs[Math.floor(Math.random() * couleurs.length)];
+    return 'green';
+  }
+
+  getBackgroundColor(){ 
+    return 'lightblue';
+  }
+
+  direBonjour(){
+    return 'Bonjour';
+  }
+
+  getRandomPersonne(){
+    return this.personnes[Math.floor(Math.random() * this.personnes.length)];
+  }
 }
---
+++ src/app/classes/personne.ts
@@ -0,0 +1,33 @@
+export class Personne {
+    constructor(
+        private _num: number,
+        private _first_name: string,
+        private _last_name: string){
+            // content
+    }
+
+    get num(){
+        return this._num;
+    }
+    set num(_num: number){
+        this._num = _num;
+    }
+
+    get firstName(){
+        return this._first_name;
+    }
+    set firstName(first_name: string){
+        this._first_name = first_name;
+    }
+
+    get lastName(){
+        return this._last_name;
+    }
+    set lastName(last_name: string){
+        this._last_name = last_name;
+    }
+    get name(){
+        return this._first_name + ' ' + this._last_name;
+    }
+
+}
